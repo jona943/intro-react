fb739256e78ee2d780c2051678305caa9dc58554
Jonathan Medina <jonathan603015@gmail.com>
2026-05-28 11:49:51 -0700

    Ejercicio lista de compras

---
+++ 2.-Manejo-de-Estado/entregable/lista-de-compras/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ListaCompras from "./componentes/listaCompras";
+
+function App() {
+  return (
+    <div className="App">
+      <ListaCompras />
+    </div>
+  );
+}
+
+export default App;
